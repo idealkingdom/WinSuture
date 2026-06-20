f0aed325ca50be4a74de2f326854eebf76480b00
idealkingdom <idealkingdom@users.noreply.github.com>
2026-06-20 18:00:00 +0800

    feat: implement disable mode for classic context menu

--- Optimizations/1_ClassicContextMenu.ps1
+++ Optimizations/1_ClassicContextMenu.ps1
@@ -1,6 +1,6 @@
 param(
     [Parameter(Mandatory=$true)]
-    [ValidateSet("Check", "Apply")]
+    [ValidateSet("Check", "Apply", "Disable")]
     [string]$Mode
 )
 
@@ -10,5 +10,9 @@ if ($Mode -eq "Check") {
 elseif ($Mode -eq "Apply") {
     New-Item -Path "HKCU:\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" -Force | Out-Null
     Set-ItemProperty -Path "HKCU:\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" -Name "(Default)" -Value "" | Out-Null
+    Stop-Process -Name explorer -Force -ErrorAction SilentlyContinue
+}
+elseif ($Mode -eq "Disable") {
+    Remove-Item -Path "HKCU:\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}" -Recurse -Force -ErrorAction SilentlyContinue | Out-Null
     Stop-Process -Name explorer -Force -ErrorAction SilentlyContinue
 }
