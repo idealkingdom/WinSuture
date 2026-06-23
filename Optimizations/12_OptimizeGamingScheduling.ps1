0afa473b589ebd56848914133c0454e806b8dfcb
idealkingdom <idealkingdom@users.noreply.github.com>
2026-06-23 09:15:00 +0800

    feat: support revert path for Gaming Scheduling tweaks

--- Optimizations/12_OptimizeGamingScheduling.ps1
+++ Optimizations/12_OptimizeGamingScheduling.ps1
@@ -1,6 +1,6 @@
 param(
     [Parameter(Mandatory=$true)]
-    [ValidateSet("Check", "Apply")]
+    [ValidateSet("Check", "Apply", "Disable")]
     [string]$Mode
 )
 
@@ -9,4 +9,7 @@ if ($Mode -eq "Check") {
 }
 elseif ($Mode -eq "Apply") {
     Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Multimedia\SystemProfile" -Name "SystemResponsiveness" -Value 0 -Type DWORD -Force | Out-Null
+}
+elseif ($Mode -eq "Disable") {
+    Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Multimedia\SystemProfile" -Name "SystemResponsiveness" -Value 20 -Type DWORD -Force | Out-Null
 }
